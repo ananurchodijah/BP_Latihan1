897e14df159dd1fed68a0ee3329c30da254b8b75
ananurchodijah <41109367+ananurchodijah@users.noreply.github.com>
2018-07-12 10:31:35 +0700

    Add files via upload

---
+++ BP.L-Rumus Persegi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int luaspersegi(int p, int l){
+	int luas;
+	luas = p*l
+	return luas;
+}
+int main()
+{
+	int a,b;
+	cout<<"masukkan panjang = ";
+	cin>>a;
+	cout<<"masukkan lebar = ";
+	cin>>b;
+	cout<<"luas persegi		="<<luaspersegi(a,b);
+	return 0;
+}
+
